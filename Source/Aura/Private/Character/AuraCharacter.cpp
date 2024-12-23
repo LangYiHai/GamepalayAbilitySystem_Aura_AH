cb00b92e93b2482ef0ab39b0265dcfa9f434853b
LYH <1311364485@qq.com>
2024-12-23 18:20:43 +0800

    4

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,28 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	// 设置角色朝移动的方向旋转
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	// 设置角色在水平旋转
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+
+	// 限制角色在水平面上移动
+	GetCharacterMovement()->bConstrainToPlane = true;
+
+	// 设置角色在开始时固定在地面
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	// 设置角色的上下旋转不由控制器控制
+	bUseControllerRotationPitch = false;
+
+	// 设置角色的翻滚不由控制器控制
+	bUseControllerRotationRoll = false;
+
+	// 设置角色的水平旋转不由控制器控制
+	bUseControllerRotationYaw = false;
+}
--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,69 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include <EnhancedInputSubsystems.h>
+#include "EnhancedInputComponent.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+
+	// 获取当前本地玩家输入子系统
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+
+	// 添加映射上下文
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	// 设置鼠标光标可见 和鼠标光标样式
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	// 设置鼠标模式为不锁定
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	// 将输入事件转换为增强事件
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	// 绑定移动事件
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	// 获取输入的二维轴值
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+
+	//获取旋转信息
+	const FRotator Rotation = GetControlRotation();
+
+	//获取当前的Yaw角度（用于计算角色或摄像机的水平方向旋转，忽略垂直方向和翻滚）
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	// 获取向前向量
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+	//获取向右方向
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>()) 
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
